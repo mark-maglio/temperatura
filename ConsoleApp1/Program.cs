4559bdb23cbf953ed94258c4a33e1a0e666104a7
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:44:07 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Serra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci la temperatura della serra: ");
+            double temperatura = double.Parse(Console.ReadLine());
+            if (temperatura < 5)
+            {
+                Console.WriteLine("Danni irreparabili");
+            }
+            else if (5 <= temperatura && temperatura < 18)
+            {
+                Console.WriteLine("Situazione di pericolo");
+            }
+            else
+            {
+                Console.WriteLine("Temperatura normale");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ temperatura/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace Serra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci la temperatura della serra: ");
+            double temperatura = double.Parse(Console.ReadLine());
+            if (temperatura < 5)
+            {
+                Console.WriteLine("la temperatura porta a danni irreparabili");
+            }
+            else if (5 <= temperatura && temperatura < 18)
+            {
+                Console.WriteLine("temperatura di pericolo");
+            }
+            else
+            {
+                Console.WriteLine("Temperatura normale");
+            }
+            Console.ReadLine();
+        }
+    }
+}
